020b11a6792bd516ca56886bc8364b9cb625e105
L4MI4 <Bhaveshmehta622@gmail.com>
2019-11-04 18:54:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ client.on('ready', () => {
   switch (command) {
     case "startloop":
       if (message.channel.timeout) return message.channel.send ('loop is already started, use []stoploop to stop the loop');
-      else { message.channel.send('Loop started, will inform you every hour now starting in'+ (Math.round(leftToFiftyFive()/60000))+ ' minutes.Use []stoploop to stop the loop');
+      else { message.channel.send('Loop started, will inform you every hour now starting in '+ (Math.round(leftToFiftyFive()/60000))+ ' minutes.Use []stoploop to stop the loop');
             message.channel.timeout=setTimeout(function(){ // in leftToFiftyFive() milliseconds run this:
             message.channel.send(new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"}));
        message.channel.loop = setInterval (() => message.channel.send (new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"})), sec * 1000)
@@ -47,9 +47,9 @@ function leftToFiftyFive(){
 });
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'curTime') {
 
-       message.reply(new Date().getMinutes());
+       message.reply('Current time in Tokyo is '+ new Date().toLocaleTimeString("jp-JP",{timeZone:"Asia/Tokyo"}));
 
        }
  });
